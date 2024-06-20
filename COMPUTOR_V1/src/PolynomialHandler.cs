27d2b30ce0a333cc928dbef7277ed8d3be66ffdf
mariekart42 <marie.a.mensing@gmail.com>
2024-06-20 13:55:14 +0200

    is reducing equation correctly

--- COMPUTOR_V1/src/PolynomialHandler.cs
+++ COMPUTOR_V1/src/PolynomialHandler.cs
@@ -23,100 +23,176 @@ public static class PolynomialHandler
 
         }
 
-
-        // x+2=0
-        // xˆ2-1=0
-        Console.WriteLine($"After Formatting: {equation}");
         return equation;
     }
 
     private static Dictionary<string, string> GetTerms(string equation)
     {
-        Dictionary<string, string> terms = new Dictionary<string, string>();
-        int equalSign = equation.IndexOf('=');
+        Dictionary<string, string> something = new Dictionary<string, string>();
+        int equalSign = GetEqualSignPosition(equation);
 
-        // Extract terms from the left side of the equation
+        List<string> terms = new List<string>();
+
         ExtractTerms(equation, 0, equalSign, terms);
-
-        // Extract terms from the right side of the equation
+        terms.Add("=");
         ExtractTerms(equation, equalSign + 1, equation.Length, terms);
 
-        // Display extracted terms for debugging
+        OrderTermsToTheLeft(terms);
+        Console.Write("Term: ");
         foreach (var term in terms)
         {
-            Console.WriteLine($"Key: {term.Key}, Value: {term.Value}");
+            Console.Write($"{term} ");
         }
+        Console.WriteLine("\n-----------");
+        SimplifyEquation(terms, something);
 
-
-
-        // int start = 0;
-        // List<string> hehe = new List<string>();
-        // int equalSign = equation.IndexOf('=');
-        // for (int i = 0; i < equalSign; i++)
+        // foreach (var some in something)
         // {
-        //     if (equation[i] == '+' || equation[i] == '-')
-        //     {
-        //         if (equation[start] == '+')
-        //             hehe.Add(equation.Substring(start+1, i - start -1));
-        //         else
-        //             hehe.Add(equation.Substring(start, i - start));
-        //         start = i;
-        //     }
+        //     Console.WriteLine($"Key: {some.Key}|Value: {some.Value}");
         // }
-        // if (equation[start] == '+')
-        //     hehe.Add(equation.Substring(start+1, equalSign - start -1));
-        // else
-        //     hehe.Add(equation.Substring(start, equalSign - start));
-        // start = equalSign + 1;
-        // for (int i = equalSign + 1; i < equation.Length; i++)
-        // {
-        //     if (equation[i] == '+' || equation[i] == '-')
-        //     {
-        //         if (equation[start] == '+')
-        //             hehe.Add(equation.Substring(start+1, i - start -1));
-        //         else
-        //             hehe.Add(equation.Substring(start, i - start));
-        //         start = i;
-        //     }
-        // }
-        // if (equation[start] == '+')
-        //     hehe.Add(equation.Substring(start+1, equation.Length - start -1));
-        // else
-        //     hehe.Add(equation.Substring(start, equation.Length - start));
-        return terms;
+
+        return something;
     }
 
-    private static void ExtractTerms(string equation, int start, int end, Dictionary<string, string> terms)
+    private static void SimplifyEquation(List<string> terms, Dictionary<string, string> termsDic)
     {
-        List<string> hehe = new List<string>();
-        int i = start;
-        string term = null;
-        while (i < end)
+        foreach (var term in terms)
         {
-            int nextSign = equation.IndexOfAny(new char[] { '+', '-' }, i, end - i);
-            if (nextSign == -1)
+            string key;
+            string replace;
+            if (!term.Contains("x") || term.Contains("xˆ0"))
             {
-                term = equation.Substring(i, end - i);
-                i = end + 1;
+                key = "constant";
+                replace = term.Contains("xˆ0") ? "xˆ0" : "";
+            }
+            else if (term.Contains("xˆ2"))
+                key = replace = "xˆ2";
+            else if (term.Contains("xˆ1") || term.Contains("x"))
+            {
+                key = "x";
+                replace = term.Contains("xˆ1") ? "xˆ1" : "x";
             }
             else
-            {
-                term = equation.Substring(i, nextSign - i);
-                i = nextSign + 1;
-            }
-            hehe.Add(term);
+                throw new Exception("AHHH. This should not happen. Delete later."); // TODO: delete later
+
+            termsDic[key] = CalculateTerms(termsDic, key, term, replace);
+            if (termsDic[key] == "0" || termsDic[key] == "-0")
+                termsDic.Remove(key);
         }
 
-        foreach (var ll in hehe)
+        Console.WriteLine("\nHEEEREEE:");
+        foreach (var lol in termsDic)
         {
-            Console.WriteLine($"term: {ll}");
+            if (lol.Key == "constant")
+                Console.Write($"{lol.Value} + ");
+            else
+                Console.Write($"{lol.Value}{lol.Key} + ");
+        }
+        Console.WriteLine(" = 0\n-------------------");
+    }
+
+    private static string CalculateTerms(Dictionary<string, string> termsDic, string key, string term2, string replace)
+    {
+        //termsDic, key, term, replace
+        if (!termsDic.ContainsKey(key))
+        {
+            // Console.WriteLine($"new entry[{key}]: {term2}");
+            if (replace == "")
+                return term2;
+            else
+            {
+                term2 = term2.Replace(replace, "");
+                if (term2 == "")
+                    return "1";
+                return term2;
+            }
         }
 
-        // Store the extracted terms in the dictionary
-        // for (int j = 0; j < hehe.Count; j++)
-        // {
-        //     terms.Add($"Term{j + 1}", hehe[j]);
-        // }
+        string term1 = termsDic[key];
+        Console.WriteLine($"Term1: {term1}");
+        Console.WriteLine($"Term1: {term2}");
+        if (replace != "")
+        {
+            term1 = term1.Replace(replace, "");
+            term2 = term2.Replace(replace, "");
+        }
+
+        if (string.IsNullOrEmpty(term1))
+            return term2;
+
+        if (string.IsNullOrEmpty(term2))
+            term2 = "1";
+        try
+        {
+            double term1_double = double.Parse(term1);
+            double term2_double = double.Parse(term2);
+            double result = term1_double + term2_double;
+            // Console.WriteLine($"calculating type[{key}] {term1_double} + {term2_double} = {result}");
+            return result + "";
+        }
+        catch (Exception e)
+        {
+            throw new Exception($"Equation is invalid. One of the following are not valid numbers: \'{term1}\', \'{term2}\'.");
+        }
+    }
+
+    private static int GetEqualSignPosition(string equation)
+    {
+        if (equation.Count(c => c == '=') > 1)
+            throw new Exception("Equation is invalid. There can only be 1 \'=\' sign.");
+        int equalSign = equation.IndexOf('=');
+        if (equalSign == -1)
+            throw new Exception("Equation is invalid. There is no \'=\' sign.");
+        return equalSign;
+    }
+
+    private static void OrderTermsToTheLeft(List<string> terms)
+    {
+        int equalSignIndex = terms.IndexOf("=");
+        for (int k = equalSignIndex + 1; k < terms.Count; k++)
+        {
+            terms[k] = NegateTerm(terms[k]);
+            // Console.WriteLine($"right side: {terms[k]}");
+        }
+
+        terms.RemoveAt(equalSignIndex); // Remove the '='
+    }
+
+    private static string NegateTerm(string term)
+    {
+        if (term.StartsWith("-"))
+            return term.Substring(1);  // Remove the '-' sign
+        else if (term.StartsWith("+"))
+            return "-" + term.Substring(1); // Change '+' to '-'
+        else
+            return "-" + term; // Prepend '-' if no sign
+    }
+
+    private static List<string> ExtractTerms(string equation, int start, int end, List<string> terms)
+    {
+        int i = start;
+        string term = null;
+        int termIndex = 1;
+        while (i < end)
+        {
+            int nextSign = equation.IndexOfAny(new char[] { '+', '-' }, i + 1, end - i - 1);
+
+            int termLength = (nextSign == -1 ? end : nextSign) - i;
+            if (equation[i] == '+')
+                term = equation.Substring(i+1, termLength-1);
+            else
+                term = equation.Substring(i, termLength);
+
+            if (!string.IsNullOrEmpty(term))
+            {
+                terms.Add(term);
+                termIndex++;
+            }
+            if (nextSign == -1)
+                break;
+            i = nextSign;
+        }
+        return terms;
     }
 
     private static int CheckExponent(string equation, int i)
